6685da7f1d527cf25efc547775d26843f40c8d5c
Ziv Peleg <zivpeleg@gmail.com>
2016-12-31 01:07:46 +0200

    Added command line interface. Can now specify playlist uri

--- getspotifyplaylist.py
+++ getspotifyplaylist.py
@@ -1,9 +1,21 @@
-"""Downloads spotify playlists by searching for them on youtube."""
+"""
+Downloads spotify playlists by searching for them on youtube.
+
+TODO:
+* Add argument parsing
+  [V] Parse Spotify Playlist URI
+  [V] Parse output folder, with sane default (./output/playlistname)
+    [ ] Read playlist name
+* Use youtube-dl as a function and not as a process
+  * https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
+"""
 
 import base64
 import json
 import subprocess
 import sys
+import argparse
+import os
 
 import requests
 from googleapiclient.discovery import build
@@ -60,8 +72,7 @@ def __uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     """
     Print unicode strings to the console.
 
-    Code from:
-    http://stackoverflow.com/a/29988426/365408
+    Code from http://stackoverflow.com/a/29988426/365408
     """
     enc = file.encoding
     if enc == 'UTF-8':
@@ -87,11 +98,12 @@ def __spotify_get_playlist_tracks(user, playlist, token):
         spotify_api_url = jsonout.get('next', '')
     return songs
 
-def __get_youtubedl_command(artist, title, youtube_link):
-    return 'youtube-dl -o "c:\\tmp\\songs\\{{artist}} - {{title}} (%(title)s).%(ext)s" -w -q -x --audio-format best --audio-quality 0 -k --prefer-ffmpeg {{youtube_link}}' \
+def __get_youtubedl_command(artist, title, youtube_link,output_folder):
+    return 'youtube-dl -o "{{outputfolder}}\\{{artist}} - {{title}} (%(title)s).%(ext)s" -w -q -x --audio-format best --audio-quality 0 -k --prefer-ffmpeg {{youtube_link}}' \
             .replace('{{artist}}', artist)\
             .replace('{{title}}', title)\
-            .replace('{{youtube_link}}', youtube_link)
+            .replace('{{youtube_link}}', youtube_link)\
+            .replace('{{outputfolder}}', output_folder)
 
 def __create_youtube_search_terms(songs):
     searchterms = []
@@ -102,10 +114,22 @@ def __create_youtube_search_terms(songs):
             searchterms.append((song, '%s %s' % (song['title'], artist_name)))
     return searchterms
 
-def __main():
+def __parse_playlist_uri(uri):
+    playlistparts = uri.split(':')
+    # Sample: spotify:user:lmljoe:playlist:0DXoY83tBvgWkd8QH49yAI
+    if len(playlistparts) != 5:
+        print("Invalid playlist id")
+        exit()
+    user_id = playlistparts[2]
+    playlist_id = playlistparts[4]
+    return user_id, playlist_id
+
+def __main(playlist, output_folder, simulate_mode):
+    user_id, playlist_id = __parse_playlist_uri(playlist)
+
     spotify_access_token = __spotify_get_access_token()
     print(' * Got access token')
-    songs = __spotify_get_playlist_tracks('oocdiddy', '2cjySDOU0ikjqjBa7JDgaV', spotify_access_token)
+    songs = __spotify_get_playlist_tracks(user_id, playlist_id, spotify_access_token)
     print(' * Got song list')
 
     searchterms = __create_youtube_search_terms(songs)
@@ -114,9 +138,19 @@ def __main():
         search_result = __youtube_search(term)
         __uprint(' * %i/%i %s - %s' % (index, len(searchterms), song['title'], str(song['artists'])))
         __uprint('   downloading: %s' % (str(search_result[0])))
-        command = __get_youtubedl_command(song['artists'][0], song['title'], search_result[0][1])
-        dl_process = subprocess.Popen(command)
-        dl_process.communicate()
+        if not simulate_mode:
+            command = __get_youtubedl_command(song['artists'][0], song['title'], search_result[0][1], output_folder)
+            dl_process = subprocess.Popen(command)
+            dl_process.communicate()
 
 if __name__ == '__main__':
-    __main()
+    # pylint: disable=C0103
+    parser = argparse.ArgumentParser()
+    parser.add_argument('playlist', help='Spotify uri of the playlist')
+    parser.add_argument('-o', '--out', help='output folder', default='.\\output')
+    parser.add_argument('-s', '--simulate', help='Do not download files, simulate process', action='store_true')
+    args = parser.parse_args()
+    print('Downloading playlist: {}\nSaving to {}'.format(args.playlist, os.path.abspath(args.out)))
+    if args.simulate:
+        print('XXX - running in simulate mode')
+    __main(args.playlist, args.out, args.simulate)
